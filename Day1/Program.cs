1531dd791618530a5b8f2921fe8b05443ba8b1d4
Joshua Duscha <joshua@duscha.dev>
2024-12-01 15:01:03 +0100

    Day 1 Part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -15,11 +15,33 @@ for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
 list1.Sort();
 list2.Sort();
 
-var distanceList = new List<int>(list1.Count);
-for (var i = 0; i < list1.Count; i++)
+Console.WriteLine(Part1(list1, list2));
+Console.WriteLine(Part2(list1, list2));
+
+
+int Part1(List<int> list1, List<int> list2)
 {
-    var distance = Math.Abs(list2[i] - list1[i]);
-    distanceList.Add(distance);
+    var distanceList = new List<int>(list1.Count);
+    for (var i = 0; i < list1.Count; i++)
+    {
+        var distance = Math.Abs(list2[i] - list1[i]);
+        distanceList.Add(distance);
+    }
+
+    return distanceList.Sum();
 }
 
-Console.WriteLine(distanceList.Sum());
+int Part2(List<int> list1, List<int> list2)
+{
+    var duplicatesList = list1.Intersect(list2).ToList();
+
+    var similarityList = new List<int>(duplicatesList.Count);
+    for (int i = 0; i < duplicatesList.Count; i++)
+    {
+        var duplicateNumber = duplicatesList[i];
+        var duplicateNumberCount = list2.FindAll(x => x == duplicateNumber).Count;
+        similarityList.Add(duplicateNumber * duplicateNumberCount);
+    }
+    
+    return similarityList.Sum();
+}
