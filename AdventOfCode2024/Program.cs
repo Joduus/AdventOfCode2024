6f69ae0be0775070ec3d610006eada065a8f96ee
Joshua Duscha <joshua@duscha.dev>
2024-12-03 14:49:44 +0100

    Select solve mode with parsed Command Line Arguments

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -1,3 +1,17 @@
 using AoCHelper;
 
-await Solver.SolveLast();
+if (args.Length == 0)
+{
+    await Solver.SolveLast();
+}
+else
+{
+    if (args[0] == "all")
+    {
+        await Solver.SolveAll();
+    }
+    else
+    {
+        await Solver.Solve(args[1..].Select(s => Convert.ToUInt32(s)).ToArray());
+    }
+}
