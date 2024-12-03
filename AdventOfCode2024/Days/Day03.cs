df0b09b56465b14850c851b9f8146f7dca4ef3a6
Joshua Duscha <joshua@duscha.dev>
2024-12-03 13:38:27 +0100

    Day 3 Part 1

--- AdventOfCode2024/Days/Day03.cs
+++ AdventOfCode2024/Days/Day03.cs
@@ -1,12 +1,26 @@
+using System.Text.RegularExpressions;
 using AoCHelper;
 
 namespace AdventOfCode2024.Days;
 
 public class Day03 : BaseDay
 {
+    private StreamReader InputFileStream => new StreamReader(File.Open(InputFilePath, FileMode.Open));
+
+    private static Regex _mulRegex = new(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled | RegexOptions.NonBacktracking | RegexOptions.CultureInvariant);
+
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        var input = InputFileStream.ReadToEnd();
+        var mulMatches = _mulRegex.Matches(input);
+
+        var mulSum = 0;
+        foreach (Match match in mulMatches)
+        {
+            mulSum += Convert.ToInt32(match.Groups[1].Value) * Convert.ToInt32(match.Groups[2].Value);
+        }
+
+        return new ValueTask<string>(mulSum.ToString());
     }
 
     public override ValueTask<string> Solve_2()
