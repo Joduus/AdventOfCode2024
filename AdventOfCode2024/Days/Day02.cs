673bcdfa48561a0f9a275d7173f60156c51e8f8d
Joshua Duscha <joshua@duscha.dev>
2024-12-05 10:27:01 +0100

    Refactor Day 2

--- AdventOfCode2024/Days/Day02.cs
+++ AdventOfCode2024/Days/Day02.cs
@@ -2,87 +2,40 @@ using AoCHelper;
 
 namespace AdventOfCode2024.Days;
 
-public class Day02 : BaseDay
+public sealed class Day02 : BaseDay
 {
-    private readonly int[][] _numberLines = new int[1000][];
+    private readonly int[][] _levelLines;
 
     public Day02()
     {
-        ParseInput();
+        _levelLines = File.ReadLines(InputFilePath).Select(line => line.Split(' ').Select(int.Parse).ToArray())
+            .ToArray();
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var safeCounter = 0;
-        foreach (var numbers in _numberLines)
-        {
-            if (IsSafe(numbers))
-            {
-                safeCounter++;
-            }
-        }
+        var safeCounter = _levelLines.Count(IsSafe);
 
         return new ValueTask<string>(safeCounter.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var safeCounter = 0;
-
-        foreach (var numbers in _numberLines)
-        {
-            if (IsSafe(numbers) || WithProblemDampener(numbers))
-            {
-                safeCounter++;
-            }
-        }
+        var safeCounter = _levelLines.Count(levels => IsSafe(levels) || WithProblemDampener(levels));
 
         return new ValueTask<string>(safeCounter.ToString());
     }
 
-    private void ParseInput()
-    {
-        var inputFile = File.Open(InputFilePath, FileMode.Open);
-        var reader = new StreamReader(inputFile);
-
-        var lineNumber = 0;
-        for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
-        {
-            var numbers = line.Split(" ").Select(x => int.Parse(x)).ToArray();
-            _numberLines[lineNumber] = numbers;
-
-            lineNumber++;
-        }
-    }
-
     bool IsSafe(int[] numbers)
     {
-        var isAscending = true;
-        var isDescending = true;
-
-        for (var i = 0; i < numbers.Length - 1; i++)
-        {
-            var diff = numbers[i + 1] - numbers[i];
-
-            if (diff is < 1 or > 3) isAscending = false;
-            if (diff is > -1 or < -3) isDescending = false;
-        }
+        var isAscending = numbers.Zip(numbers.Skip(1)).All(pair => pair.Second - pair.First is >= 1 and <= 3);
+        var isDescending = numbers.Zip(numbers.Skip(1)).All(pair => pair.First - pair.Second is >= 1 and <= 3);
 
         return isAscending || isDescending;
     }
 
     bool WithProblemDampener(int[] numbers)
     {
-        for (var i = 0; i < numbers.Length; i++)
-        {
-            var modifiedNumbers = numbers.Where((_, index) => index != i).ToArray();
-
-            if (IsSafe(modifiedNumbers))
-            {
-                return true;
-            }
-        }
-
-        return false;
+        return numbers.Select((_, i) => numbers.Where((_, index) => index != i).ToArray()).Any(IsSafe);
     }
 }
