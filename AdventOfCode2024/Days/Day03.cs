101367207fdd85948e84b084ca2968554cebce18
Joshua Duscha <joshua@duscha.dev>
2024-12-05 10:47:56 +0100

    Refactor Day 3

--- AdventOfCode2024/Days/Day03.cs
+++ AdventOfCode2024/Days/Day03.cs
@@ -12,52 +12,43 @@ public sealed class Day03 : BaseDay
         RegexOptions.Compiled | RegexOptions.NonBacktracking | RegexOptions.CultureInvariant);
 
     private static Regex _mulRegexWithInstructions = new(@"(mul(?=\((\d+),(\d+)\))|do(?:n't)?(?=\(\)))",
-        RegexOptions.Compiled | RegexOptions.CultureInvariant);
+        RegexOptions.Compiled  | RegexOptions.CultureInvariant);
 
     public Day03()
     {
-        var inputFileStream = File.Open(InputFilePath, FileMode.Open);
-        _input = new StreamReader(inputFileStream).ReadToEnd();
+        _input = File.ReadAllText(InputFilePath);
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var mulMatches = _mulRegexSimple.Matches(_input);
-
-        var mulSum = 0;
-        foreach (Match match in mulMatches)
-        {
-            mulSum += Convert.ToInt32(match.Groups[1].Value) * Convert.ToInt32(match.Groups[2].Value);
-        }
+        var mulSum = _mulRegexSimple.Matches(_input)
+            .Sum(match => Convert.ToInt32(match.Groups[1].Value) * Convert.ToInt32(match.Groups[2].Value));
 
         return new ValueTask<string>(mulSum.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var instructionMatches = _mulRegexWithInstructions.Matches(_input);
-
-        var mulSum = 0;
-        var multiplicationAllowed = true;
-        foreach (Match match in instructionMatches)
+        var mulSum = _mulRegexWithInstructions.Matches(_input)
+            .Aggregate((sum: 0, multiplicationAllowed: true), (acc, match) =>
         {
             switch (match.Groups[1].Value)
             {
                 case "do":
-                    multiplicationAllowed = true;
+                        acc.multiplicationAllowed = true;
                     break;
                 case "don't":
-                    multiplicationAllowed = false;
+                        acc.multiplicationAllowed = false;
                     break;
                 case "mul":
-                    if (multiplicationAllowed)
-                    {
-                        mulSum += Convert.ToInt32(match.Groups[2].Value) * Convert.ToInt32(match.Groups[3].Value);
-                    }
-
-                    break;
+                        if (acc.multiplicationAllowed)
+                        {
+                            acc.sum += Convert.ToInt32(match.Groups[2].Value) * Convert.ToInt32(match.Groups[3].Value);
+                        }
+                        break;
             }
-        }
+                return acc;
+            }).sum;
 
         return new ValueTask<string>(mulSum.ToString());
     }
