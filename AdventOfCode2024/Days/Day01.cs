7d841ac9a90348a0ef16937f44a4fcbdf27724ce
Joshua Duscha <joshua@duscha.dev>
2024-12-05 09:23:39 +0100

    Refactor Day 1

--- AdventOfCode2024/Days/Day01.cs
+++ AdventOfCode2024/Days/Day01.cs
@@ -2,58 +2,31 @@ using AoCHelper;
 
 namespace AdventOfCode2024.Days;
 
-public class Day01 : BaseDay
+public sealed class Day01 : BaseDay
 {
     private readonly List<int> _leftList = [];
     private readonly List<int> _rightList = [];
 
     public Day01()
     {
-        ParseInput();
+        _leftList = File.ReadLines(InputFilePath).Select(x => int.Parse(x.AsSpan(0, 5))).ToList();
+        _rightList = File.ReadLines(InputFilePath).Select(x => int.Parse(x.AsSpan(8, 5))).ToList();
+
+        _leftList.Sort();
+        _rightList.Sort();
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var totalDistance = 0;
-        for (var i = 0; i < _leftList.Count; i++)
-        {
-            var distance = Math.Abs(_rightList[i] - _leftList[i]);
-            totalDistance += distance;
-        }
+        var totalDistance = _leftList.Zip(_rightList, (left, right) => Math.Abs(left - right)).Sum();
 
         return new ValueTask<string>(totalDistance.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var duplicatesList = _leftList.Intersect(_rightList).ToList();
-
-        var totalSimilarity = 0;
-        for (var i = 0; i < duplicatesList.Count; i++)
-        {
-            var duplicateNumber = duplicatesList[i];
-            var duplicateNumberCount = _rightList.FindAll(x => x == duplicateNumber).Count;
-            totalSimilarity += duplicateNumber * duplicateNumberCount;
-        }
+        var totalSimilarity = _leftList.Intersect(_rightList).Sum(duplicateNumber => duplicateNumber * _rightList.Count(x => x == duplicateNumber));
 
         return new ValueTask<string>(totalSimilarity.ToString());
-    }
-
-    private void ParseInput()
-    {
-        var inputFile = File.Open(InputFilePath, FileMode.Open);
-        var reader = new StreamReader(inputFile);
-
-        for (var line = reader.ReadLine(); line != null; line = reader.ReadLine())
-        {
-            var list1Number = line.AsSpan(0, 5);
-            var list2Number = line.AsSpan(8, 5);
-
-            _leftList.Add(int.Parse(list1Number));
-            _rightList.Add(int.Parse(list2Number));
-        }
-
-        _leftList.Sort();
-        _rightList.Sort();
     }
 }
