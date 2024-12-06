2473e48cb290c46366148d30ee7c9ea2e97ee0c3
Joshua Duscha <joshua@duscha.dev>
2024-12-06 13:26:34 +0100

    Add Day 6

---
+++ AdventOfCode2024/Days/Day06.cs
@@ -0,0 +1,16 @@
+using AoCHelper;
+
+namespace AdventOfCode2024.Days;
+
+public class Day06 : BaseDay
+{
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
