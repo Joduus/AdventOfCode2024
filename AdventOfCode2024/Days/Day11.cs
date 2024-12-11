d2d0fb61a04889b6a728abc186779a258adda444
Joshua Duscha <joshua@duscha.dev>
2024-12-11 19:09:54 +0100

    Day 11 Part 2

--- AdventOfCode2024/Days/Day11.cs
+++ AdventOfCode2024/Days/Day11.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using AoCHelper;
 
 namespace AdventOfCode2024.Days;
@@ -5,33 +6,56 @@ namespace AdventOfCode2024.Days;
 public sealed class Day11 : BaseDay
 {
     private const int Part1Blinks = 25;
+    private const int Part2Blinks = 75;
 
     private readonly long[] _input;
 
     public Day11()
     {
-        _input = File.ReadLines(InputFilePath).SelectMany(s => s.Split(' ').Select(long.Parse)).ToArray();
+        _input = File.ReadLines(InputFilePath)
+            .SelectMany(s => s.Split(' ').Select(long.Parse))
+            .ToArray();
     }
 
-    public override ValueTask<string> Solve_1()
+    public override ValueTask<string> Solve_1() => new(CountStones(_input, Part1Blinks).ToString());
+
+    public override ValueTask<string> Solve_2() => new(CountStones(_input, Part2Blinks).ToString());
+
+    private static long CountStones(long[] startingStones, int blinks)
     {
-        var stones = _input.ToList();
-        for (var i = 0; i < Part1Blinks; i++)
+        var memory = new Dictionary<(long number, int blinks), long>();
+        return startingStones.Sum(l => ProcessStone(l, blinks - 1, memory));
+    }
+
+    private static long ProcessStone(long stone, int blinks, IDictionary<(long number, int blinks), long> memory)
+    {
+        var stones = ApplyRules(stone);
+
+        if (blinks == 0) return stones.Length;
+
+        if (memory.TryGetValue((stone, blinks), out var value)) return value;
+
+        var sum = 0L;
+        foreach (var newStone in stones)
         {
-            stones = stones.SelectMany(stone => stone switch
-            {
-                0 => [1],
-                _ => stone.ToString().Length % 2 == 0
-                    ? [int.Parse(stone.ToString().AsSpan(0, stone.ToString().Length / 2)), int.Parse(stone.ToString().AsSpan(stone.ToString().Length / 2))]
-                    : new[] {stone * 2024}
-            }).ToList();
+            sum += ProcessStone(newStone, blinks - 1, memory);
         }
 
-        return new ValueTask<string>(stones.Count.ToString());
+        memory[(stone, blinks)] = sum;
+
+        return sum;
     }
 
-    public override ValueTask<string> Solve_2()
-    {
-        throw new NotImplementedException();
-    }
+    private static long[] ApplyRules(long stone) =>
+        stone switch
+        {
+            0 => new[] { 1L },
+            _ => stone.ToString().Length % 2 == 0
+                ? new[]
+                {
+                    long.Parse(stone.ToString().AsSpan(0, stone.ToString().Length / 2)),
+                    long.Parse(stone.ToString().AsSpan(stone.ToString().Length / 2))
+                }
+                : new[] { stone * 2024 }
+        };
 }
